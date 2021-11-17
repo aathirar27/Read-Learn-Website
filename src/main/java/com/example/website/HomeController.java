7ec0229bcf673f5ec096199fe36d8963ff577f2c
Athira Rajeev <athirage@gmail.com>
2021-11-17 00:13:33 -0500

    firsy commit

---
+++ src/main/java/com/example/website/HomeController.java
@@ -0,0 +1,31 @@
+package com.example.website;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+
+    @GetMapping("/services")
+    public String services(){
+
+        return "services";
+    }
+
+
+}
